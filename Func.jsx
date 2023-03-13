b241e82ae92e2f7f98243dbb51976bfa3a655a94
Syeda-Sabreen <122090342+Syeda-Sabreen@users.noreply.github.com>
2023-03-13 16:11:58 +0530

    first_react_app
    my first react app

---
+++ Func.jsx
@@ -0,0 +1,61 @@
+let Func = () => {
+    let hero_Name = "Vicky Kaushal"
+    let hero_Image = "https://th.bing.com/th/id/OIP.-8xSsOndZaZEj9UGqyBhcwHaFj?w=230&h=180&c=7&r=0&o=5&pid=1.7"
+    let hero_Name1 = "Vijay Deverkonda"
+    let hero_Image1 = "https://th.bing.com/th/id/OIP.jbFAb2FFo3nwkzbrTpGA8gHaLH?w=204&h=306&c=7&r=0&o=5&pid=1.7"
+  let hero_Name2 = "Arjun Kapoor"
+    let hero_Image2 = "https://th.bing.com/th/id/OIP.E7VpnsdBErYC9PtIC9exdgHaHH?w=177&h=180&c=7&r=0&o=5&pid=1.7"
+    let hero_Name3 = "Dulquer Salmaan"
+    let hero_Image3 = "https://th.bing.com/th/id/OIP.Orkgew2bTqSXR8jRAMOXIAAAAA?w=178&h=263&c=7&r=0&o=5&pid=1.7"
+
+    return (
+        
+       
+        <div className="container">
+            <div className="row">
+                <div className="col-md-3 mt-4">
+                    <div className="card">
+                        <div className="card-header">
+                            <img src={hero_Image} alt="" />
+                        </div>
+                        <div className="card-body">
+                            <h3>{hero_Name}</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3 mt-4">
+                    <div className="card">
+                        <div className="card-header">
+                            <img src={hero_Image1} alt="" />
+                        </div>
+                        <div className="card-body">
+                            <h3>{hero_Name1}</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3 mt-4">
+                    <div className="card">
+                        <div className="card-header">
+                            <img src={hero_Image2} alt="" />
+                        </div>
+                        <div className="card-body">
+                            <h3>{hero_Name2}</h3>
+                        </div>
+                    </div>
+                </div>  
+                <div className="col-md-2 mt-4"> {/* Adjusted the column width to 2 */}
+                    <div className="card">
+                        <div className="card-header">
+                            <img src={hero_Image3} alt="" />
+                        </div>
+                        <div className="card-body">
+                            <h3>{hero_Name3}</h3>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </div>    
+    )
+}
+
+export default Func;
