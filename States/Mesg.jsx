e84276bb698470994bd417a5e072647bc4c39c8c
Syeda-Sabreen <122090342+Syeda-Sabreen@users.noreply.github.com>
2023-03-20 23:56:01 +0530

    Add files via upload

---
+++ States/Mesg.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class Mesg extends Component {
+
+    constructor(props) {
+        super(props)
+        console.log("First Const")
+        this.state = { msg: "Hello" }
+    }
+    gmHandler = () => {
+        console.log("if user clicks - ")
+        this.setState({ msg: "Good Morning" })
+    }
+    componentDidMount() {
+        console.log("third - life cycle method -automataicallly")
+    }
+    render() {
+        console.log("second render method")
+        return (
+            <div>
+                <pre>{JSON.stringify(this.state)}</pre>
+                <h2>Message:{this.state.msg}</h2>
+                <button onClick={this.gmHandler}>GM</button>
+
+            </div>
+        )
+    }
+}
+
+export default Mesg;
